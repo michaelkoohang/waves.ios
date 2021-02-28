4bf9af582f3609c09d376dae6da3d68471292603
Michael Koohang <michael.s.k1001@gmail.com>
2021-02-28 11:32:27 -0500

    Add UserDefaults manager

---
+++ Waves/UserDefaultsManager.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+struct UserDefaultsManager {
+    static private let defaults = UserDefaults.standard
+    static private let LOGIN_KEY = "logged_in"
+    
+    static func setLoggedIn(status: Bool) {
+        defaults.setValue(status, forKey: LOGIN_KEY)
+    }
+    
+    static func getLoggedIn() -> Bool {
+        return defaults.bool(forKey: LOGIN_KEY)
+    }
+    
+}
